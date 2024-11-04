d0a7a55b690bff38d4ff97678c1174bff441ad9d
Nafis Khalilov <nafis5559@gmail.com>
2024-11-04 03:08:48 +0300

    add healthcheck

--- src/web-api/Program.cs
+++ src/web-api/Program.cs
@@ -3,11 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+
+app.UseHealthChecks("/health");
 
 app.UseAuthorization();
 
